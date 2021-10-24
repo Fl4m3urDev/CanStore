e25e682f5e42c201ff4800bbb829c61b66c60abc
Banaban <alban.le_pape@gretacotesarmor.fr>
2021-10-24 20:31:25 +0200

    +com

--- produits_tri.php
+++ produits_tri.php
@@ -6,14 +6,14 @@ try {
   $utilisateur = 'root'; // root sur vos postes
   $motDePasse = ''; // pas de mot de passe sur vos postes
   $connexion = new PDO( $dns, $utilisateur, $motDePasse );
-  $connexion->exec('SET NAMES utf8');
+  $connexion->exec('SET NAMES utf8');//recuperer en utf8
 } catch (Exception $e) {
   echo "Connexion à MySQL impossible : ", $e->getMessage();
   die();
 }
-//print_r($_POST['category']);
+//base de la requete
 $requete = "SELECT * FROM produits";   
-
+// si une categorie choisie trier
 if($_POST['category']!='Tous')
 {
     $requete .= " WHERE  type = '".strtolower($_POST['category'])."'"; 
@@ -21,8 +21,9 @@ if($_POST['category']!='Tous')
 
 // Envoi de la requête vers MySQL
 $select = $connexion->query($requete);
+//recu sous forme d'un seul tableau et transformation en Json
 $produits = json_encode($select->fetchAll( PDO::FETCH_ASSOC ));
-
+// affichage du Json
 print_r($produits);
 
 ?>
